0dbea4f9e5ab51ecfee9fb0b3e9011bb29c0507f
Mahdi Bahrami <mahdi.bahrami74@gmail.com>
2023-03-31 19:40:03 +0330

    Adding mining, creating and loading in node

--- wallet.py
+++ wallet.py
@@ -24,8 +24,10 @@ class Wallet:
                     f.write(self.public_key)
                     f.write('\n')
                     f.write(self.private_key)
+                return True
             except (IOError, IndexError):
                 print('Saving wallet faild!')
+                return False
 
 
     def load_keys(self):
@@ -36,9 +38,10 @@ class Wallet:
                 private_key = keys[1]
                 self.public_key = public_key
                 self.private_key = private_key
+            return True
         except (IOError, IndexError):
             print('Loading wallet faild!')
-
+            return False
 
     def generate_keys(self):
         private_key = RSA.generate(1024, Crypto.Random.new().read)
